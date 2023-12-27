7f10bf8ba35257fb003e51f727ca1c9bf8fa23d6
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2023-12-27 22:09:22 +0700

    feat: initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# MF-Airflow
+
+A set of tools of orchestrating ETL data.
---
+++ airflow/dags/process_input_data.py
@@ -0,0 +1,53 @@
+import pathlib
+
+import pendulum
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.exceptions import AirflowSkipException
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
+from airflow.providers.sftp.hooks.sftp import SFTPHook
+from airflow.providers.sftp.operators.sftp import SFTPOperation, SFTPOperator
+
+INPUT_FILE_PATH = "/sftp/test.txt"
+LOCAL_TMP_PATH = "/tmp/test.txt"
+
+with DAG(
+    "process_input_data",
+    start_date=pendulum.datetime(2023, 12, 27, tz="Asia/Ho_chi_minh"),
+    schedule="*/1 * * * *",
+) as dag:
+
+    @task()
+    def check_for_sftp_file():
+        previous_processing_date: pendulum.datetime.DateTime = pendulum.parse(  # type: ignore
+            Variable.get("sftp_prev_ts", None)
+        )
+
+        sftp_hook: SFTPHook = BaseHook.get_hook("sftp_conn")  # type: ignore
+
+        file_mod_timestamp = sftp_hook.get_mod_time(INPUT_FILE_PATH)
+        file_mod_time = pendulum.from_format(
+            file_mod_timestamp, "YYYYMMDDHHmmss", tz="Asia/Ho_chi_minh"
+        )
+
+        if previous_processing_date and previous_processing_date >= file_mod_time:
+            raise AirflowSkipException("SFTP file has not been modified")
+
+        Variable.set("sftp_prev_ts", file_mod_time)
+
+    download_sftp_files = SFTPOperator(
+        task_id="download_sftp_files",
+        sftp_hook=BaseHook.get_hook("sftp_conn"),  # type: ignore
+        operation=SFTPOperation.GET,
+        remote_filepath=INPUT_FILE_PATH,
+        local_filepath=LOCAL_TMP_PATH,
+    )
+
+    check_for_sftp_file() >> download_sftp_files
+
+
+if __name__ == "__main__":
+    file_path = pathlib.Path(__file__).parent
+    dag.test(conn_file_path=str(file_path / "../connections.json"))
